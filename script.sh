330f15acf0a30de1fa09378416253ceb4b6f66b4
AthenaHTA2 <sedmakh2@gmail.com>
2022-06-25 11:12:43 +0100

    script

--- script.sh
+++ script.sh
@@ -1,13 +1,12 @@
-(!/bin/sh)
-
-sudo docker stop $(docker ps -a -q)
-sudo docker rm $(docker ps -a -q)
+#!/bin/bash
+# sudo docker stop $(docker ps -a -q)
+# sudo docker rm $(docker ps -a -q)
 sudo docker system prune -a
-sudo docker y
+y
 
-sudo docker build -t dockerforum .
-# sudo docker container run -p 8080:8080 -d --name containerdockerforum dockerforum
+sudo docker build -t binarycolour .
+sudo docker container run -p 8080:8080 -d --name containerbinarycolour binarycolour
 sudo docker image ls
 sudo docker ps -a
-sudo docker exec -it containerdockerforum /bin/bash
+sudo docker exec -it containerbinarycolour /bin/bash
 sudo ls -l
